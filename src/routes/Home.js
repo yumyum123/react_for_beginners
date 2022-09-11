b2d8828ebe361056e14eb0d95876be537af011d1
장윤희 <dbsgml9325@etoos.com>
2022-09-12 02:02:57 +0900

    year 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -39,6 +39,7 @@ function Home(){
                 <Movie 
                     key = {movie.id}
                     id = {movie.id}
+                    yaer = {movie.year}
                     coverImg = {movie.medium_cover_image}
                     title = {movie.title}
                     summary = {movie.summary}
