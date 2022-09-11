68d675a0e2756ba6be89fb9fcc034575693d1ca0
장윤희 <dbsgml9325@etoos.com>
2022-09-12 01:55:13 +0900

    css 추가

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import './style.css';
+import './styles.css';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect, useState } from "react";
 import Movie from '../components/Movie';
+import styles from "./Home.module.css"
 
 function Home(){
     const [loading, setLoading] = useState(true);
@@ -27,11 +28,13 @@ function Home(){
         getMovies();
     }, []);
     return (
-        <div>
+        <div className={styles.container}>
         {loading ? (
-            <h1>loading,,</h1>
+            <div className={styles.loader}>
+                <h1>loading,,</h1>
+            </div>
         ) : (
-            <div>
+            <div className={styles.movies}>
             {movies.map((movie) => (
                 <Movie 
                     key = {movie.id}
